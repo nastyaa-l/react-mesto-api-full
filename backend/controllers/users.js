607a6114484f51189cb2efd220ee3609da651d50
Anastasia Lyugaeva <nastya.lyugaeva@bk.ru>
2021-09-03 11:30:07 +0300

    refac auth

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -87,6 +87,9 @@ const updateUserAvatar = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
+    .catch(() => {
+      throw new AuthError('Требуется авторизация');
+    })
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
